0a849deecabc9c63a77abf8b683c0f76f5f51d5d
Joshua Smerdel <josh@smerdel.net>
2021-01-28 17:45:47 -0500

    Hello World course files

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,16 @@
+public class Hello {
+
+    public static void main(String[] args){
+        System.out.println("Hello, Joshua Smerdel");
+
+        int firstNum = (40 + 5) + (2 * 10);
+        int secondNum = firstNum * firstNum;
+        int thirdNum = secondNum * firstNum;
+        int fourthNum = thirdNum * firstNum;
+        int lastOne = fourthNum - 1000;
+
+        System.out.println(fourthNum + "," + lastOne);
+    }
+}
+
+
