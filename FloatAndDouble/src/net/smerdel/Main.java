5d58e6c36f62a63028e9788dd872717a47a13f1e
Joshua Smerdel <josh@smerdel.net>
2021-01-28 20:38:46 -0500

    nightly push 01: 1/28/21

---
+++ FloatAndDouble/src/net/smerdel/Main.java
@@ -0,0 +1,26 @@
+package net.smerdel;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    double minDoubleVal = Double.MIN_VALUE;
+	    double maxDoubleVal = Double.MAX_VALUE;
+	    float minFloatVal = Float.MIN_VALUE;
+	    float maxFloatVal = Float.MAX_VALUE;
+        int intVal1 = 5 / 3;
+        float floatVal1 = 5f / 3f;
+        double doubleVal1 = 5d / 3d;
+
+        System.out.println("minDoubleVal: " + minDoubleVal);
+        System.out.println("maxDoubleVal: " + maxDoubleVal);
+        System.out.println("minFloatVal: " + minFloatVal);
+        System.out.println("maxFloatVal: " + maxFloatVal);
+        System.out.println("intVal: " + intVal1);
+        System.out.println("floatVal: " + floatVal1);
+        System.out.println("doubleVal: " + doubleVal1);
+
+
+
+
+    }
+}
